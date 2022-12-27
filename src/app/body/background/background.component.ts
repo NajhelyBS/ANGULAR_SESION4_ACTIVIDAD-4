af8abc03278c297a4cd1f6a707f9bcbb0b702b34
NajhelyBS <s5anbandasanchez@gmail.com>
2022-12-27 11:24:37 -0500

    Nueva comunicación agregada

--- src/app/body/background/background.component.ts
+++ src/app/body/background/background.component.ts
@@ -7,6 +7,50 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class BackgroundComponent {
 
+  urlImage:String = "https://cdn.shopify.com/s/files/1/0337/9586/6760/products/il_fullxfull.3495327464_8hjj_9643b5c5-f221-4dc9-ae7c-dcfd1b0e1edb_1024x1024.jpg?v=1641910587";
+
+
+  cambiarUrl1(){
+    var personaje = document.getElementsByTagName("td");
+
+    this.urlImage = "https://genshin.global/wp-content/uploads/2022/12/faruzab-character-genshin-impact.webp";
+
+
+  };
+ 
+  cambiarUrl2(){
+    
+    var personaje = document.getElementsByTagName("td");
+ 
+    this.urlImage = "https://genshin.global/wp-content/uploads/2022/05/xiao-anemo-profile-genshin-impact.webp";
+
+  };
+
+
+cambiarUrl3(){
+  var personaje = document.getElementsByTagName("td");
+
+  this.urlImage = "https://genshin.global/wp-content/uploads/2020/09/chongyun-cryo-profile-genshin-impact.webp";
+
+
+};
+
+cambiarUrl4(){
+  var personaje = document.getElementsByTagName("td");
+
+  this.urlImage = "https://genshin.global/wp-content/uploads/2022/06/barbara-hydro-profile-genshin-impact.webp";
+
+  
+
+};
+cambiarUrl5(){
+  var personaje = document.getElementsByTagName("td");
+
+  this.urlImage = "https://genshin.global/wp-content/uploads/2022/10/nilou-hydro-avatar-profile-genshin-impact.webp";
+
+
+};
+
 
 }
 
--- src/app/body/container1/container1.component.ts
+++ src/app/body/container1/container1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container1',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Container1Component {
 
-  urlImage:String = "https://cdn.shopify.com/s/files/1/0337/9586/6760/products/il_fullxfull.3495327464_8hjj_9643b5c5-f221-4dc9-ae7c-dcfd1b0e1edb_1024x1024.jpg?v=1641910587";
+  @Input() urlImage:String = "https://cdn.shopify.com/s/files/1/0337/9586/6760/products/il_fullxfull.3495327464_8hjj_9643b5c5-f221-4dc9-ae7c-dcfd1b0e1edb_1024x1024.jpg?v=1641910587";
 
 // FARUZAN urlImage = "https://genshin.global/wp-content/uploads/2022/12/faruzab-character-genshin-impact.webp";
 
